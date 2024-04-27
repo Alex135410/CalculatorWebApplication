18bb63e0aa66e4918a4f7af96059f3acffb610cb
Joel Roche <joelroche23@gmail.com>
2024-04-26 23:30:13 -0400

    Taking out calculator web from the Application

--- src/calculator-web/src/main/java/org/example/calculatorweb/CalculatorWebApplication.java
+++ src/calculator-web/src/main/java/org/example/calculatorweb/CalculatorWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "org.example.calculatorweb")
+@SpringBootApplication(scanBasePackages = "org.example")
 @RestController
 public class CalculatorWebApplication {
 
